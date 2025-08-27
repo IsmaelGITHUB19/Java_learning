8f709e65962b12bf64e4b0da98f2f8d5f1c92b9b
Ismael <ISMAELELGER@HOTMAIL.COM>
2025-08-27 17:13:02 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+
+public class Main {
+    private static final String String = null;
+
+    public static void main(String[] args) {
+        // Criando vetor de livros
+        livro [] livros = {
+            new livro("O Senhor dos Anéis", "J.R.R. Tolkien", 1954, 4.9, 50.0),
+            new livro("1984", "George Orwell", 1949, 4.8, 30.0),
+            new livro("Duna", "Frank Herbert", 1965, 4.7, 45.0),
+            new livro("Neuromancer", "William Gibson", 1984, 4.5, 40.0),
+            new livro("Fahrenheit 451", "Ray Bradbury", 1953, 4.6, 35.0)
+        };
+        animal rato = new animal("Rato", 20, "Roedor");
+        System.out.println(" Nome " + rato.getNome() + " Idade " + rato.getIdade() + " Especie " + rato.getEspecie());
+       pessoa ismael = new pessoa("Ismael",20,"Masculino");
+        System.out.println(ismael.getNome() + " " + ismael.getIdade() + " " + ismael.getsexo());
+       
+       pessoa [] cadastros = {
+            new pessoa("Julio", 50, "Masculino"),
+            new pessoa ("Amanda", 20, "Feminino"),
+       };
+       System.out.println(cadastros[0].getNome() + " " + cadastros[1].getIdade() + " ");
+       
+       funcionario Felipe = new funcionario("Felipe", 33, "Masculino", "Matutino - Vespertino", 3500);
+       System.out.println(Felipe.getNome()+ " " + Felipe.getIdade() + " " + Felipe.getsexo() + " " + Felipe.getSalario() + " " + Felipe.getHorario()); 
+        
+       
+       
+        // Rotina de ordenação considerando a ordem natural (por título)
+        
+         //  Arrays.sort(livros);
+       //System.out.println("--- Ordenação por Título (Ordem Natural) ---");
+       //for (livro livro : livros) {
+         //  System.out.println(livro);
+       //}
+       // Rotina de ordenação considerando o ano de publicação
+
+       // Rotina de ordenação considerando o autor
+
+       // Rotina de ordenação considerando o custo-benefício
+    }
+ }
