c9c6a280fe1cb29b3ace66ec038c1ccc6552b5fc
Ismael <ISMAELELGER@HOTMAIL.COM>
2025-09-02 16:33:01 -0300

    Update JavaSortLivroClass

--- Livro.java
+++ Livro.java
@@ -1,28 +1,27 @@
-public class livro implements Comparable<livro> {
-    private String titulo;
+public class livros {
+    private String nome;
     private String autor;
-    private Integer ano;
+    private Integer anop;
     private Double nota;
     private Double preco;
 
-    public livro(String titulo, String autor, Integer ano, Double nota, Double preco) {
-        this.titulo = titulo;
+    public livros (String nome, String autor, Integer anop, Double nota, Double preco){
+        this.nome = nome;
         this.autor = autor;
-        this.ano = ano;
+        this.anop = anop;
         this.nota = nota;
         this.preco = preco;
     }
-
-    public String getTitulo() {
-        return titulo;
+    public String getNome() {
+        return nome;
     }
 
     public String getAutor() {
         return autor;
     }
 
-    public int getAno() {
-        return ano;
+    public int getAnop() {
+        return anop;
     }
 
     public double getNota() {
@@ -31,15 +30,5 @@ public class livro implements Comparable<livro> {
 
     public double getPreco() {
         return preco;
-    }
-
-    @Override
-    public String toString() {
-        return titulo + ", " + autor + ", " + ano + ", " + nota + ", " + preco;
-    }
-
-    @Override
-    public int compareTo(livro outro) {
-        return this.titulo.compareTo(outro.titulo);
     }
 }
